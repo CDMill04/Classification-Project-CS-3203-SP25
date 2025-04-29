efe98a268a39705f9c58e917778df8e6ed5ae497
kyle443 <Clouse1123@gmail.com>
2025-04-29 13:55:32 -0500

    adding back notifications to school and redownloading nodemailer

--- app/school/page.tsx
+++ app/school/page.tsx
@@ -7,6 +7,7 @@ import useCurrentUser from "@/app/hooks/useCurrentUser";
 import LoginModal from "@/app/components/modals/loginPage";
 import SignUpModal from "@/app/components/modals/SignUpPage";
 import { getAllSchools, getAllUsers, createSchool, joinSchool, updateUser, promoteToAdmin, getLessonPlansForSchool } from "./actions";
+import Notification from "@/app/components/Notification";
 
 
 export default function SchoolManagement() {
@@ -23,7 +24,7 @@ export default function SchoolManagement() {
   const [isSignUpOpen, setSignUpOpen] = useState(false);
   const [logoutMessage, setLogoutMessage] = useState(false);
 
-  
+  const [notification, setNotification] = useState<string | null>(null);
   
     const openLogin = () => {
       setLoginOpen(true);
@@ -57,6 +58,7 @@ export default function SchoolManagement() {
       setMySchool(currentUser.school);
       setView(currentUser.role === "admin" ? "admin" : "teacher");
       loadLessonPlans();
+      setNotification("School Successfully Created!");
     }
     setLoading(false);
   };
@@ -77,6 +79,7 @@ export default function SchoolManagement() {
     setMyRole("teacher");
     setMySchool(selectedSchool);
     setView("teacher");
+    setNotification("Successfully joined " + selectedSchool + "!");
   };
 
   const loadSchools = async () => {
@@ -218,6 +221,10 @@ export default function SchoolManagement() {
           </div>
         )}
       </div>
+
+      {/* Show Notification */}
+      {notification && <Notification message={notification} onClose={() => setNotification(null)} />}
+      
       {/* Login and Sign Up Modals */}
       {isMounted && (
         <>
